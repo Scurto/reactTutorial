91634401b62078a67c3d87516a2476e5bfceaf8e
scurto <1234567890>
2021-09-01 14:51:30 +0300

    setState, local state(71)

--- src/components/profile/posts/profileInfo/ProfileInfo.jsx
+++ src/components/profile/posts/profileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import css from './ProfileInfo.module.css';
 import Preloader from "../../../common/Preloader";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -10,6 +11,7 @@ const ProfileInfo = (props) => {
     return (
         <div className={css.content}>
             <img src={props.profile.photos.large != null ? props.profile.photos.large : 'https://vokrug-tv.ru/pic/person/2/b/f/4/2bf448098b7badf3b37e87c510da29bc.jpeg'}/>
+            <ProfileStatus status={"Hello test"}/>
             <div>About me: {props.profile.aboutMe}</div>
             <div>Full name: {props.profile.fullName}</div>
             <div>User id: {props.profile.userId}</div>
