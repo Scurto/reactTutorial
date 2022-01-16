9fdea767d15d1f56173df97de450ef341f4aa8af
scurto <1234567890>
2022-01-16 11:35:53 +0200

    hook and useState(84)

--- src/components/profile/posts/profileInfo/ProfileInfo.jsx
+++ src/components/profile/posts/profileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import css from './ProfileInfo.module.css';
 import Preloader from "../../../common/Preloader";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHook from "./ProfileStatusWithHook";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -11,7 +12,8 @@ const ProfileInfo = (props) => {
     return (
         <div className={css.content}>
             <img src={props.profile.photos.large != null ? props.profile.photos.large : 'https://vokrug-tv.ru/pic/person/2/b/f/4/2bf448098b7badf3b37e87c510da29bc.jpeg'}/>
-            <ProfileStatus status={ props.status } updateUserStatus={ props.updateUserStatus }/>
+            {/*<ProfileStatus status={ props.status } updateUserStatus={ props.updateUserStatus }/>*/}
+            <ProfileStatusWithHook status={ props.status } updateUserStatus={ props.updateUserStatus }/>
             <div>About me: {props.profile.aboutMe}</div>
             <div>Full name: {props.profile.fullName}</div>
             <div>User id: {props.profile.userId}</div>
